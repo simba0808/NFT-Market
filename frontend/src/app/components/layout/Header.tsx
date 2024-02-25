655e091e22025cbea77c5ae75c29c486fd463764
simba0808 <simba960808@gmail.com>
2024-02-25 15:29:17 +1000

    feat: add ERC721 contract, fix: component import change in frontend

--- frontend/src/app/components/layout/Header.tsx
+++ frontend/src/app/components/layout/Header.tsx
@@ -99,7 +99,7 @@ const Header: React.FC = () => {
                 <>
                   <ConnectButton showBalance={true} />
                   <button onClick={handleMenuClick} >
-                    <Image src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=6GHAjsWpt9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80" className="w-10 h-10 rounded-full" alt="avatar" />
+                    <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixqx=6GHAjsWpt9&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80" className="w-10 h-10 rounded-full" alt="avatar" />
                   </button>
                   {
                     logoutShow &&
